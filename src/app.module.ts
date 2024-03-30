57013ab21cd9b3daa073e74a458ec97e6568d047
Kevin Arias <aa-kevin@hotmail.com>
2024-03-30 07:42:35 -0600

    Add HealthModule and HealthController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { initEnvVariables, initTypeOrmModule } from './config';
 import { CodeModule } from './code/code.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [initEnvVariables, initTypeOrmModule, CodeModule],
+  imports: [initEnvVariables, initTypeOrmModule, CodeModule, HealthModule],
 })
 export class AppModule {}
